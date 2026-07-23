d282129bc45e5dc8952960af43afdc1c23243eac
LQ <43512109+LQ-Epoch@users.noreply.github.com>
2026-07-24 03:49:15 +0800

    Integrate vendored Schwab HTTP transport

--- vendor/schwab-api-nodejs/src/utils/version.ts
+++ vendor/schwab-api-nodejs/src/utils/version.ts
@@ -1,6 +1,2 @@
-import pkg from '../../package.json' with { type: 'json' };
-
-const version = typeof pkg.version === 'string' && pkg.version.length > 0 ? pkg.version : '0.0.0';
-
-export const SDK_VERSION = version;
-export const DEFAULT_USER_AGENT = `schwab-owokit/${version} (+https://github.com/schwab-owokit/schwab-api-nodejs)`;
+export const SDK_VERSION = '1.0.0';
+export const DEFAULT_USER_AGENT = `schwab-auto-bot-sdk/${SDK_VERSION}`;
